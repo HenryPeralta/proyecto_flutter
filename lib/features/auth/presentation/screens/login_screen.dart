e5717cd7c2c0d08af9016288cc071e5b0cabdf29
HenryPeralta <henrygabrielperalta@gmail.com>
2026-03-08 11:37:28 -0600

    Agregando login principal

---
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../widgets/login_header.dart';
+import '../widgets/login_form.dart';
+import '../widgets/login_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login() {
+
+    String username = usernameController.text;
+    String password = passwordController.text;
+
+    if (username == "admin" && password == "12345") {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login correcto ✅"),
+        ),
+      );
+
+    } else {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Usuario o contraseña incorrectos ❌"),
+        ),
+      );
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              const LoginHeader(),
+
+              const SizedBox(height: 30),
+
+              LoginForm(
+                usernameController: usernameController,
+                passwordController: passwordController,
+              ),
+
+              const SizedBox(height: 30),
+
+              LoginButton(
+                onTap: login,
+              ),
+
+              const SizedBox(height: 40),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
